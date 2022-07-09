412b257b397af3944fcbd8cef5b3f828c3e26d16
castlemin999 <castlemin999@gmail.com>
2022-07-09 11:57:44 +0900

    old Controller

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 // 하위 폴더에 있는 Servlet을 찾아서 등록
 @ServletComponentScan
@@ -13,4 +16,13 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+
+	// ViewResolver 설정
+	// SpringBoot에서는 application.properties에서 설정 가능하다.
+	/*
+	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
+	*/
 }
